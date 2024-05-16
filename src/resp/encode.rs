77a4b9d5a4cbdf24c21d0d293bcd516cd718a389
hedon954 <171725713@qq.com>
2024-05-17 00:06:45 +0800

    feat: defind `RespEncode` and `RespDecode` traits and implement `RespEncode` trait for `RespFrame`.

---
+++ src/resp/encode.rs
@@ -0,0 +1,214 @@
+use crate::{
+    BulkString, RespArray, RespEncode, RespMap, RespNull, RespNullArray, RespNullBulkString,
+    RespSet, SimpleError, SimpleString,
+};
+
+const BUF_CAP: usize = 4096;
+
+/// Simple strings are encoded as a plus (+) character, followed by a string.
+/// The string mustn't contain a CR (\r) or LF (\n) character and is terminated by CRLF (i.e., \r\n).
+///
+/// Examples: +OK\r\n
+impl RespEncode for SimpleString {
+    fn encode(self) -> Vec<u8> {
+        format!("+{}\r\n", self.0).into_bytes()
+    }
+}
+
+/// Simple errors, or simply just errors, are similar to simple strings,
+/// but their first character is the minus (-) character.
+///
+/// The difference between simple strings and errors in RESP is
+/// that clients should treat errors as exceptions,
+/// whereas the string encoded in the error type is the error message itself.
+///
+/// Examples: -Error message\r\n
+impl RespEncode for SimpleError {
+    fn encode(self) -> Vec<u8> {
+        format!("-{}\r\n", self.0).into_bytes()
+    }
+}
+
+/// The null bulk string represents a non-existing value.
+/// The `GET`` command returns the Null Bulk String when the target key doesn't exist.
+///
+/// Examples: $-1\r\n
+impl RespEncode for RespNullBulkString {
+    fn encode(self) -> Vec<u8> {
+        b"$-1\r\n".to_vec()
+    }
+}
+
+/// Null arrays exist as an alternative way of representing a null value.
+/// For instance, when the BLPOP command times out, it returns a null array.
+///
+/// Examples: *-1\r\n
+impl RespEncode for RespNullArray {
+    fn encode(self) -> Vec<u8> {
+        b"*-1\r\n".to_vec()
+    }
+}
+
+/// The null data type represents non-existent values.
+///
+/// Examples: _\r\n
+impl RespEncode for RespNull {
+    fn encode(self) -> Vec<u8> {
+        b"_\r\n".to_vec()
+    }
+}
+
+/// This type is a CRLF-terminated string that represents a signed, base-10, 64-bit integer.
+///
+/// Format:
+///     :[<+|->]<value>\r\n
+///
+/// - The colon (:) as the first byte.
+/// - An optional plus (+) or minus (-) as the sign.
+/// - One or more decimal digits (0..9) as the integer's unsigned, base-10 value.
+/// - The CRLF terminator.
+impl RespEncode for i64 {
+    fn encode(self) -> Vec<u8> {
+        let sign = if self < 0 { "" } else { "+" };
+        format!(":{}{}\r\n", sign, self).into_bytes()
+    }
+}
+
+/// A bulk string represents a single binary string.
+/// The string can be of any size, but by default,
+/// Redis limits it to 512 MB (see the proto-max-bulk-len configuration directive).
+///
+/// Format:
+///     $<length>\r\n<data>\r\n
+///
+/// - The dollar sign ($) as the first byte.
+/// - One or more decimal digits (0..9) as the string's length, in bytes, as an unsigned, base-10 value.
+/// - The CRLF terminator.
+/// - The data.
+/// - A final CRLF.
+impl RespEncode for BulkString {
+    fn encode(self) -> Vec<u8> {
+        let mut buf = Vec::with_capacity(self.len() + 16);
+        buf.extend_from_slice(&format!("${}\r\n", self.len()).into_bytes());
+        buf.extend_from_slice(&self);
+        buf.extend_from_slice(b"\r\n");
+        buf
+    }
+}
+
+/// Clients send commands to the Redis server as RESP arrays.
+/// Similarly, some Redis commands that return collections of
+/// elements use arrays as their replies.
+///
+/// Format:
+///     *<number-of-elements>\r\n<element-1>...<element-n>
+///
+/// - An asterisk (*) as the first byte.
+/// - One or more decimal digits (0..9) as the number of elements in the array as an unsigned, base-10 value.
+/// - The CRLF terminator.
+/// - An additional RESP type for every element of the array.
+impl RespEncode for RespArray {
+    fn encode(self) -> Vec<u8> {
+        let mut buf = Vec::with_capacity(BUF_CAP);
+        buf.extend_from_slice(format!("*{}\r\n", self.len()).as_bytes());
+        for frame in self.0 {
+            buf.extend_from_slice(&frame.encode())
+        }
+        buf
+    }
+}
+
+/// #<t|f>\r\n
+impl RespEncode for bool {
+    fn encode(self) -> Vec<u8> {
+        format!("#{}\r\n", if self { "t" } else { "f" }).into_bytes()
+    }
+}
+
+/// The Double RESP type encodes a double-precision floating point value.
+/// Format:
+///     ,[<+|->]<integral>[.<fractional>][<E|e>[sign]<exponent>]\r\n
+///
+/// - The comma character (,) as the first byte.
+/// - An optional plus (+) or minus (-) as the sign.
+/// - One or more decimal digits (0..9) as an unsigned, base-10 integral value.
+/// - An optional dot (.), followed by one or more decimal digits (0..9) as an unsigned, base-10 fractional value.
+/// - An optional capital or lowercase letter E (E or e),
+///     followed by an optional plus (+) or minus (-) as the exponent's sign,
+///     ending with one or more decimal digits (0..9) as an unsigned, base-10 exponent value.
+/// - The CRLF terminator.
+///
+/// Example:
+///     1.23
+///     ,1.23\r\n
+///
+/// Other examples:
+///     ,inf\r\n
+///     ,-inf\r\n
+///     ,nan\r\n
+impl RespEncode for f64 {
+    fn encode(self) -> Vec<u8> {
+        let mut buf = Vec::with_capacity(32);
+        let ret = if self.abs() > 1e+8 || self.abs() < 1e-8 {
+            format!(",{:e}\r\n", self)
+        } else {
+            let sign = if self < 0.0 { "" } else { "+" };
+            format!(",{}{}\r\n", sign, self)
+        };
+
+        buf.extend_from_slice(&ret.into_bytes());
+        buf
+    }
+}
+
+/// The RESP map encodes a collection of key-value tuples, i.e., a dictionary or a hash.
+/// Format:
+///     %<number-of-entries>\r\n<key-1><value-1>...<key-n><value-n>
+///
+/// - A percent character (%) as the first byte.
+/// - One or more decimal digits (0..9) as the number of entries, or key-value tuples, in the map as an unsigned, base-10 value.
+/// - The CRLF terminator.
+/// - Two additional RESP types for every key and value in the map.
+///
+/// Examples:
+///     {
+///         "first": 1,
+///         "second": 2
+///     }
+///            ↓
+///         %2\r\n
+///         +first\r\n
+///         :1\r\n
+///         +second\r\n
+///         :2\r\n
+/// (The raw RESP encoding is split into multiple lines for readability).
+impl RespEncode for RespMap {
+    fn encode(self) -> Vec<u8> {
+        let mut buf = Vec::with_capacity(BUF_CAP);
+        buf.extend_from_slice(&format!("%{}\r\n", self.len()).into_bytes());
+        for (key, value) in self.0 {
+            buf.extend(SimpleString::new(key).encode());
+            buf.extend(&value.encode());
+        }
+        buf
+    }
+}
+
+/// Sets are somewhat like Arrays but are unordered and should only contain unique elements.
+/// Format:
+///     ~<number-of-elements>\r\n<element-1>...<element-n>
+///
+/// - A tilde (~) as the first byte.
+/// - One or more decimal digits (0..9) as the number of elements in the set as an unsigned, base-10 value.
+/// - The CRLF terminator.
+/// - An additional RESP type for every element of the Set.
+impl RespEncode for RespSet {
+    fn encode(self) -> Vec<u8> {
+        let mut buf = Vec::with_capacity(BUF_CAP);
+        buf.extend_from_slice(&format!("~{}\r\n", self.len()).into_bytes());
+        for frame in self.0 {
+            buf.extend_from_slice(&frame.encode());
+        }
+        buf
+    }
+}
---
+++ src/resp/mod.rs
@@ -0,0 +1,118 @@
+use std::{
+    collections::{HashMap, HashSet},
+    ops::Deref,
+};
+
+use enum_dispatch::enum_dispatch;
+
+pub mod decode;
+pub mod encode;
+
+#[enum_dispatch]
+pub trait RespEncode {
+    fn encode(self) -> Vec<u8>;
+}
+
+pub trait RespDecode {
+    fn decode(buf: Self) -> Result<RespFrame, String>;
+}
+
+/// RESP(Redis serialization protocol specification).
+/// According to https://redis.io/docs/latest/develop/reference/protocol-spec/.
+#[enum_dispatch(RespEncode)]
+pub enum RespFrame {
+    SimpleString(SimpleString),
+    Error(SimpleError),
+
+    NullBulkString(RespNullBulkString),
+    NullArray(RespNullArray),
+    Null(RespNull),
+
+    Integer(i64),
+    BulkString(BulkString),
+    Array(RespArray),
+    Boolean(bool),
+    Double(f64),
+    Map(RespMap),
+    Set(RespSet),
+}
+
+pub struct SimpleString(String);
+pub struct SimpleError(String);
+pub struct RespNullBulkString;
+pub struct RespNullArray;
+pub struct RespNull;
+pub struct RespArray(Vec<RespFrame>);
+pub struct BulkString(Vec<u8>);
+pub struct RespMap(HashMap<String, RespFrame>);
+pub struct RespSet(HashSet<RespFrame>);
+
+impl SimpleString {
+    pub fn new(s: impl Into<String>) -> Self {
+        SimpleString(s.into())
+    }
+}
+
+impl SimpleError {
+    pub fn new(s: impl Into<String>) -> Self {
+        SimpleError(s.into())
+    }
+}
+
+impl RespArray {
+    pub fn new(s: impl Into<Vec<RespFrame>>) -> Self {
+        RespArray(s.into())
+    }
+}
+
+impl RespMap {
+    pub fn new() -> Self {
+        RespMap(HashMap::new())
+    }
+}
+
+impl RespSet {
+    pub fn new() -> Self {
+        RespSet(HashSet::new())
+    }
+}
+
+impl Default for RespMap {
+    fn default() -> Self {
+        RespMap::new()
+    }
+}
+
+impl Default for RespSet {
+    fn default() -> Self {
+        RespSet::new()
+    }
+}
+
+impl Deref for BulkString {
+    type Target = Vec<u8>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for RespArray {
+    type Target = Vec<RespFrame>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for RespMap {
+    type Target = HashMap<String, RespFrame>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for RespSet {
+    type Target = HashSet<RespFrame>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
